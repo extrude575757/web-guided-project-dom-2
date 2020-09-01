0b853d81fda69c8a6e4cdaa23637c8c848af3044
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-01 19:11:08 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ Array.from(document.links).forEach(function (link) {
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
 launchButton.addEventListener('click', function (event) {
-  
+  // absolute power over the entire DOM
+  modal.classList.remove('off')
 })
 
 // 👉 TASK 4- Create a function to confirm the launch.
