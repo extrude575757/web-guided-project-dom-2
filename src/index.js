f1c415291ec86c8aa01cd93abb895a99c5d6df50
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-01 19:10:18 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -42,7 +42,9 @@ Array.from(document.links).forEach(function (link) {
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
-
+launchButton.addEventListener('click', function (event) {
+  
+})
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
