4c371c4f2e433fa0f65f6ab7eda3c519e71b9f67
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-01 19:13:31 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ Array.from(document.links).forEach(function (link) {
 // Add it as an event listener for click events on the launch button.
 launchButton.addEventListener('cl1ck', function (event) {
   // absolute power over the entire DOM
+  debugger
   modal.classList.remove('off')
 })
 
